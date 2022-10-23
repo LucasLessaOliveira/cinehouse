f8d7eb9d2927e97842bba8869c5a8c6fa3fed6d3
LucasLessa <oliverlucas983@gmail.com>
2022-10-23 19:50:52 -0300

    Primeira versão do projeto cinehouse

---
+++ cinema.js
@@ -0,0 +1,11 @@
+const catalogo = require('./database/catalogo.json')
+
+
+
+
+//listar d=todos os filmes que estão em cartaz(listarTodosOsFilmesEmCartaz)
+
+//buscar um filme específico pelo código dele (buscarFilme)  -> parametro : codigo
+
+//alterar o status emCartaz ( true -> false e se tiver false -> true)
+//(alterarStatusEmCartaz) -> parametro : codigo
